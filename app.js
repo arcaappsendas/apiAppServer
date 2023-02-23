f3789ac349aba2a533b996a79fbbcf58b2c7442a
Darwin Cabezas <desarrollo.04@torresytorres.com>
2023-02-23 10:12:44 -0500

    Correccion

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ var array = [];
 app.post('/log', (req, res) => {
   const logMessage = req.body.message;
   array.push(req.body);
-  console.log(JSON.stringify(array));
-  res.send(JSON.stringify(array));
-  //res.status(204).end();
- 
+  console.log(JSON.stringify(req.body));
+  res.send(JSON.stringify(req.body));
 });
+
+app.get('/getlog', (req, res) => {
+    res.send(JSON.stringify(array));
+  });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
