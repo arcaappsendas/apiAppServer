6bf667d2d83ca0fa5a663641a13e4c4edfa9ba03
Darwin Cabezas <desarrollo.04@torresytorres.com>
2023-02-23 09:13:46 -0500

    PRIMER COMMIT

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/log', (req, res) => {
+  const logMessage = req.body.message;
+  console.log(logMessage);
+  res.status(204).end();
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
